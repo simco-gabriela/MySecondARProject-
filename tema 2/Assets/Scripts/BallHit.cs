aa42d24a74e1c16238771f1b4b8316749e5df482
simco-gabriela <gabrielasimco@yahoo.com>
2024-10-15 21:34:15 +0300

    golf club and ball interaction

---
+++ tema 2/Assets/Scripts/BallHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BallHit : MonoBehaviour
+{
+    public Rigidbody ballRigidbody;  
+    private float forceMultiplier = 100f;  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("mingeee");
+            Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (ballRb != null)
+            {
+                Vector3 clubVelocity = GetComponent<Rigidbody>().velocity;
+
+                Vector3 hitDirection = collision.contacts[0].normal;
+
+                Vector3 force = -hitDirection * clubVelocity.magnitude * forceMultiplier;
+                ballRb.AddForce(force, ForceMode.Impulse);  
+            }
+        }
+    }
+}
